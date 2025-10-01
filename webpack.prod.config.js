9f63f5232ab3e6f00c0bfee724c9720fe62c3279
Sleeping Dude <ivanh@wingspan.app>
2025-09-30 19:01:59 -0700

    Comment out CleanWebpackPlugin in production webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,9 +5,9 @@ const configs = require('./webpack.config');
 configs.forEach(config => {
     config.mode = 'production';
     config.output.path = path.resolve(`./build/${process.env.BROWSER}/prod`);
-    config.plugins.push(
-        new CleanWebpackPlugin()
-    );
+    // config.plugins.push(
+    //     new CleanWebpackPlugin()
+    // );
 });
 
 module.exports = configs;
